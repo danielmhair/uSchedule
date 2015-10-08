8c9d8e4fe14024e5602b626749fbdd05d2398dd4
Daniel Hair <danielmhair@gmail.com>
2015-10-07 21:20:47 -0600

    fixed errors and added boards, lists, and cards to the one page app.

--- assets/js/todoController.js
+++ assets/js/todoController.js
@@ -7,29 +7,85 @@ myapp.controller('todoCtrl', function ($scope) {
   
   var onAuthorize = function() {
   	updateLoggedIn();
-  	$("#cards").empty();
-  	console.log("updated login");
+  	$("#boards").empty();
   	Trello.members.get("me", function(member){
   		$("#fullName").text(member.fullName);
-  		var $cards = $("<div>")
-  			.text("Loading Cards...")
-  			.appendTo("#cards");
+  		getBoardsWithLists();
+  	});
+  };
+  
+  var getBoardsWithLists = function() {
+	  var $boards = $("<div>")
+  			.text("Loading Boards...")
+  			.appendTo("#boards");
   		// Output a list of all of the cards that the member 
   		// is assigned to
-  		Trello.get("members/me/cards", function(cards) {
-  			$cards.empty();
-  			$.each(cards, function(ix, card) {
-  				$("<div><a>")
-  				.attr({href: card.url, target: "trello"})
-  				.addClass("card")
-  				.text(card.name)
-  				.appendTo($cards);
-  			});  
-			console.log(cards);
+	  Trello.get("members/me/boards", function(boards) {
+  			$boards.empty();
+  			$.each(boards, function(ix, board) {
+  				$("<div>")
+					.attr({ id: board.id })
+					.addClass("board")
+					.appendTo($boards);
+				$("<h2>")
+					.attr({ id: "title-" + board.id })
+					.appendTo("#" + board.id); 
+				$("<a>")
+					.attr({ href: board.url, target: "trello" })
+					.text(board.name)
+					.appendTo("#title-" + board.id);
+				
+				getListsWithCards();
+				
+  			});
   		});
-  	});
+  }
   
-  };
+  var getListsWithCards = function() {
+	var $lists = $("<div>")
+		.text("Loading Lists...")
+		.appendTo("#" + board.id);
+	Trello.get("boards/" + board.id + "/lists", function(lists) {
+		$lists.empty();
+		$.each(lists, function(ix, list) {
+			$("<div>")
+				.attr({ id: list.id })
+				.addClass("list")
+				.appendTo($lists);
+			$("<h3>")
+				.attr({ id: "list-title-" + list.id })
+				.appendTo("#" + list.id); 
+			$("<a>")
+				.attr({ href: list.url, target: "trello" })
+				.text(list.name)
+				.appendTo("#list-title-" + list.id);
+			getCards();
+		}); 
+	}, function(error) { console.log(error); });
+  }
+  
+  var getCards = function() {
+	var $cards = $("<div>")
+		.text("Loading Cards...")
+		.appendTo("#" + list.id);
+	Trello.get("lists/" + list.id + "/cards", function(cards) {
+		
+		$.each(cards, function(ix, card) {
+			$("<div>")
+				.attr({ id: card.id })
+				.addClass("card")
+				.appendTo($lists);
+			$("<p>")
+				.attr({ id: "card-title-" + card.id })
+				.appendTo("#" + card.id); 
+			$("<a>")
+				.attr({ href: card.url, target: "trello" })
+				.text(card.name)
+				.appendTo("#card-title-" + card.id);
+		});
+		
+	}, function(error) { console.log(error); });
+  }
   
   var updateLoggedIn = function() {
   	var isLoggedIn = Trello.authorized();
@@ -41,9 +97,10 @@ myapp.controller('todoCtrl', function ($scope) {
   $scope.authorize = function() {
 	var authenticationSuccess = function() { 
 		onAuthorize();
+		document.getElementById("header").style.display = "block !important";
 	};
 	var authenticationFailure = function() { 
-		console.log("Failed authentication"); 
+		
 	};
 	
 	Trello.authorize({
@@ -62,7 +119,8 @@ myapp.controller('todoCtrl', function ($scope) {
   $scope.logout = function() {
   	Trello.deauthorize();
   	updateLoggedIn();
-  };
+	document.getElementById("header").style.display = "none !important";
+};
   						  
   //Trello.authorize({
 	//  type: "popup",
